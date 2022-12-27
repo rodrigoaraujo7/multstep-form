2da6da0e64d63da76728226411b071f78345c021
Rodrigo Leite Araujo <dev.rodrigoaraujo@gmail.com>
2022-12-27 20:57:56 -0300

    react-icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import { GrFormNext, GrFormPrevious } from 'react-icons/gr'
+
 import './App.css'
 
 export const App = () => {
   return (
-    <h1>Hello World!</h1>
+    <div className="app">
+      <div className="header">
+        <h2>Rate this product</h2>
+        <p>We happy with your order, use the under form to rate this product</p>
+      </div>
+      <div className="form-container">
+        <p>steps</p>
+        <form>
+          <div className="actions">
+            <button type='button'>
+              <GrFormPrevious />
+              <span>Return</span>
+            </button>
+            <button type='submit'>
+              <span>Next</span>
+              <GrFormNext />
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
