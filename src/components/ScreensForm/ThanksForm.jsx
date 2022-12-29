f6a680a9946a87fd91eacb5e390e0c2d1cc0db2c
Rodrigo Leite Araujo <dev.rodrigoaraujo@gmail.com>
2022-12-29 00:53:05 -0300

    thanks page

--- src/components/ScreensForm/ThanksForm.jsx
+++ src/components/ScreensForm/ThanksForm.jsx
@@ -1,5 +1,28 @@
+import { 
+    BsFillEmojiHeartEyesFill,
+    BsFillEmojiSmileFill,
+    BsFillEmojiNeutralFill,
+    BsFillEmojiFrownFill
+} from 'react-icons/bs'
+
+import './Thanks.css'
+
 export const ThanksForm = () => {
     return (
-        <h2>Thanks Form</h2>
+        <div className='thanks-container'>
+            <h2>Wait a minute...</h2>
+            <p>
+                Your opinion is a lot of important, later you'll 
+                receive a 10% discount coupon to you next buy
+            </p>
+            <p>To complete your review, click the submit button below </p>
+            <h3>Review Resume</h3>
+            <p className="review-data">
+                <span>Product satisfaction</span>
+            </p>
+            <p className="review-data">
+                <span>Comments: </span>
+            </p>
+        </div>
     )
 }
