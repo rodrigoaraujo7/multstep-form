fbe949531c96ce0d631b0fc8001afb1c35ba3d86
Rodrigo Leite Araujo <dev.rodrigoaraujo@gmail.com>
2022-12-28 23:21:55 -0300

    form template & style

--- src/components/ScreensForm/UserForm.jsx
+++ src/components/ScreensForm/UserForm.jsx
@@ -1,5 +1,26 @@
 export const UserForm = () => {
     return (
-        <h2>UserForm</h2>
+        <div>
+            <div className="form-control">
+                <label htmlFor="name">Name:</label>
+                <input 
+                    type="text" 
+                    name="name" 
+                    id="name" 
+                    placeholder="Place your name" 
+                    required 
+                />
+            </div>
+            <div className="form-control">
+                <label htmlFor="email">Email:</label>
+                <input 
+                    type="email" 
+                    name="email" 
+                    id="email" 
+                    placeholder="Place your name" 
+                    required 
+                />
+            </div>
+        </div>
     )
 }
