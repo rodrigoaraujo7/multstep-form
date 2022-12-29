5e2b4bb9c232cb506d8a79da37f5772def55e82b
Rodrigo Leite Araujo <dev.rodrigoaraujo@gmail.com>
2022-12-29 00:17:47 -0300

    review components

--- src/components/ScreensForm/ReviewForm.jsx
+++ src/components/ScreensForm/ReviewForm.jsx
@@ -1,5 +1,46 @@
+import { 
+    BsFillEmojiHeartEyesFill,
+    BsFillEmojiSmileFill,
+    BsFillEmojiNeutralFill,
+    BsFillEmojiFrownFill
+} from 'react-icons/bs'
+
+import './ReviewForm.css';
+
 export const ReviewForm = () => {
     return (
-        <h2>Review Form</h2>
+        <div className="review-form">
+            <div className="form-control score-container">
+                <label className="radio-container">
+                    <input type="radio" value="unsatisfied" name="review" required />
+                    <BsFillEmojiFrownFill />
+                    <p>Unsatisfied</p>
+                </label>
+                <label className="radio-container">
+                    <input type="radio" value="neutral" name="review" required />
+                    <BsFillEmojiNeutralFill />
+                    <p>Neutral</p>
+                </label>
+                <label className="radio-container">
+                    <input type="radio" value="satisfied" name="review" required />
+                    <BsFillEmojiSmileFill />
+                    <p>Satisfied</p>
+                </label>
+                <label className="radio-container">
+                    <input type="radio" value="very_satisfied" name="review" required />
+                    <BsFillEmojiHeartEyesFill />
+                    <p>Very Satisfied</p>
+                </label>
+            </div>
+            <div className="form-control">
+                <label htmlFor="comment">Comment</label>
+                <textarea 
+                    name="comment"
+                    id="comment"
+                    placeholder='Comment your experience with us system'
+                    required
+                > </textarea>
+            </div>
+        </div>
     )
 }
